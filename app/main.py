1d9f42b7f920de19047f243e4917c4843b13e25b
wilsonmedina <pemdastau@gmail.com>
2022-12-05 12:16:13 -0300

    practice

--- app/main.py
+++ app/main.py
@@ -5,28 +5,33 @@ import pandas as pd
 
 def run():
   '''
-  df = pd.read_csv('data.csv')
-  df = df[df['Continent'] == 'Africa']
   data = list(filter(lambda item : item['Continent'] == 'South America',data))
-
-  countries = df['Country'].values
-  percentages = df['World Population Percentage'].values
-  charts.generate_pie_chart(countries, percentages)
-  '''
-  data = read_csv.read_csv('data.csv')
-  
   countries = list(map(lambda x: x['Country'], data))
   percentages = list(map(lambda x: x['World Population Percentage'], data))
   
+  df = df[df['Continent'] == 'Africa']
+  
+  percentages = df['World Population Percentage'].values
+  charts.generate_pie_chart(countries, percentages)
+  df = pd.read_csv('data.csv')
+  data = df['Country'].values
+  '''
 
-  country =  input('Type Country => ').capitalize()
+  
+  
+  data = read_csv.read_csv('data.csv')
+  country = input('Type Country => ').capitalize()
+  print(country)
+  
 
   result = utils.population_by_country(data, country)
 
   if len(result) > 0:
     country = result[0]
+    print(country)
     labels, values = utils.get_population(country)
     charts.generate_bar_chart(country['Country'], labels, values)
 
 if __name__ == '__main__':
   run()
+
